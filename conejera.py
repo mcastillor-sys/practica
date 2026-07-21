d91d0f44e8e0cf4b602533dd2085645e02aa019e
mcastillor-sys <mcastillor@ucenfotec.ac.cr>
2026-07-20 20:39:50 -0600

    new: Python doc

---
+++ conejera.py
@@ -0,0 +1,9 @@
+cant_jaulas = int(input ("Digite la cantidad de jaulas: "))
+cant_conejos_jaula = int(input("Digite la cantidad de conejos por jaula: "))
+dosis_vacuna = 2.5
+
+# Cálculos
+total_conejos = cant_jaulas * cant_conejos_jaula
+total_dosis = total_conejos * dosis_vacuna
+
+print("El total de dosis es de " , total_dosis)
